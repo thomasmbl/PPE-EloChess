4d2fba77c86402d157dde5ba96db40051147e404
thomasmbl <91416520+thomasmbl@users.noreply.github.com>
2023-02-10 11:34:11 +0100

    Add files via upload

--- frontend/src/Pages/Admin.js
+++ frontend/src/Pages/Admin.js
@@ -2,14 +2,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import './Admin.css';
-import { Link } from "react-router-dom";
-import { TonalitySharp } from "@mui/icons-material";
 
-
-const init2 = {
-    saisonNumber:"",
-    deathsNumber:"",
-}
 
 
 const init = {
@@ -23,18 +16,21 @@ const init = {
     maison: "",
     nbPartiesJoue: "",
     nbPartiesGagne: "",
-    idEcole: ""
+    idEcole: "",
+    mdp: ""
 }
 
 const Admin = () => {
     const [data, setdata] = useState([]);
     const [state, setState] = useState(init);
-    const [state2, setState2] = useState(init2);
-
-    const { saisonNumber, deathsNumber } = state2;
-
 
     const { nom, prenom, section, classe, pseudo, mdp, elo, maison, nbPartiesJoue, nbPartiesGagne, idEcole } = state;
+   
+
+    useEffect(() => {
+        getEleves();
+    }, [])
+
 
     const getEleves = async () => {
         const res = await axios.get("http://localhost:3001/eleves")
@@ -43,41 +39,22 @@ const Admin = () => {
         }
     };
 
-    useEffect(() => {
+    const addEleve = async (e) => {
+        await axios.post("http://localhost:3001/eleves", e);
+        
         getEleves();
-    }, [])
-
-
-
-
-
-    /*
-    CRUD Methods
-    */
-    
-    const getSaisons = async () => {
-        const res = await axios.get("http://localhost:3001/saisons")
-        if (res.status === 200) {
-            setdata(res.data);
-        }
-    };
-    
-
-     
-    const addSaison = async (e) => {
-        await axios.post("http://localhost:3001/saisons", e);
-        getSaisons();
     };
 
-    const deleteSaison = async (id) => {
-        await axios.delete(`http://localhost:3001/saisons/${id}`)
-        getSaisons();
+    const deleteEleve = async (id) => {
+        await axios.delete(`http://localhost:3001/eleves/${id}`)
+        getEleves();
     };
 
-    const updateSaison = async (id) => {
-        await axios.patch(`http://localhost:3001/saisons/${id}`, state)
-        getSaisons();
-    }
+    const updateEleve = async (id) => {
+        console.error(nom);
+        await axios.patch(`http://localhost:3001/eleves/${id}`, state)
+        getEleves();
+    };
 
 
     const handleInputChange = (e) => {
@@ -87,28 +64,12 @@ const Admin = () => {
     
     const handleSubmitAdd = (e) => {
         e.preventDefault();
-        if(!saisonNumber || !deathsNumber) {
-            console.error("Empty field", saisonNumber, deathsNumber)
+        if(!nom || !prenom || !section || !classe || !pseudo || !elo || !maison || !nbPartiesJoue || !nbPartiesGagne || !idEcole || !mdp) {
+            console.error("Empty field")
         } else {
-            addSaison(state);
+            addEleve(state);
         }
     }
-
-    const handleSubmitEdit = (e) => {
-        e.preventDefault();
-        if(!saisonNumber || !deathsNumber) {
-            console.error("Empty field", saisonNumber, deathsNumber)
-        } else {
-            updateSaison(state);
-        }
-    }
-
-    /*
-    useEffect(() => {
-        getSaisons();
-    }, [])
-    */
-    
 
     
     
@@ -150,8 +111,8 @@ const Admin = () => {
 
 
                                 <td>
-                                    <button className="btn-delete" onClick={() => deleteSaison(item._id)}>Delete</button>
-                                    <button className="btn-edit" onClick={() => updateSaison(item._id)}>Update</button>
+                                    <button className="btn-delete" onClick={() => deleteEleve(item._id)}>Delete</button>
+                                    <button className="btn-edit" onClick={() => updateEleve(item._id)}>Update</button>
                                 </td>
                             </tr>
                            
@@ -161,12 +122,12 @@ const Admin = () => {
             </table>
             <div>
                 <form>
-                    <label>Prénom</label>
-                    <input placeholder="Prénom" id="prénom" name="prénom" onChange={handleInputChange} value={prenom}></input>
-
                     <label>Nom</label>
                     <input placeholder="Nom" id="nom" name="nom" onChange={handleInputChange} value={nom}></input>
 
+                    <label>Prénom</label>
+                    <input placeholder="Prénom" id="prenom" name="prenom" onChange={handleInputChange} value={prenom}></input>
+                    
                     <label>Section</label>
                     <input placeholder="Section" id="section" name="section" onChange={handleInputChange} value={section}></input>
 
@@ -191,18 +152,9 @@ const Admin = () => {
                     <label>ID école</label>
                     <input placeholder="Id école" id="idEcole" name="idEcole" onChange={handleInputChange} value={idEcole}></input>
 
+                    <label>mdp</label>
+                    <input placeholder="Mot de passe" id="mdp" name="mdp" onChange={handleInputChange} value={mdp}></input>
 
-
-
-
-
-
-
-                    <label>Season Number</label>
-                    <input placeholder="Season Number" id="saisonNumber" name="saisonNumber" onChange={handleInputChange} value={saisonNumber}></input>
-
-                    <label>Death Count</label>
-                    <input placeholder="Death Count" name="deathsNumber" onChange={handleInputChange} value={deathsNumber}></input>
 
                     <input type="submit" value="Add" onClick={handleSubmitAdd}></input>
                 </form>
