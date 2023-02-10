d5558f212c70ef12438a44a1f60e90f3190d3152
thomasmbl <91416520+thomasmbl@users.noreply.github.com>
2023-02-10 11:35:05 +0100

    Add files via upload

--- backend/routes/eleves.js
+++ backend/routes/eleves.js
@@ -14,23 +14,11 @@ router.get('/', async (req, res) => {
 })
 
 //Get One
-router.get('/:id', (req, res) => {
-    
+router.get('/:id', getEleve, async (req, res) => {
+    res.json(res.eleve)
 })
 
 //Create One
-/*
-section: req.body.section,
-        classe: req.body.classe,
-        pseudo: req.body.pseudo,
-        mdp: req.body.mdp,
-        elo: req.body.elo,
-        maison: req.body.maison,
-        nbPartiesJoue: req.body.nbPartiesJoue,
-        nbPartiesGagne: req.body.nbPartiesGagne,
-        idEcole: req.body.idEcole,
-
-*/
 router.post('/', async (req, res) => {
     const eleve = new Eleve({
         nom: req.body.nom,
@@ -55,16 +43,72 @@ router.post('/', async (req, res) => {
 })
 
 //Update One
-router.patch('/:id', (req, res) => {
-   
+router.patch('/:id', getEleve, async (req, res) => {
+    if(req.body.nom != null) {
+        res.eleve.nom = req.body.nom
+    }
+    if(req.body.prenom != null) {
+        res.eleve.prenom = req.body.prenom
+    }
+    if(req.body.section != null) {
+        res.eleve.section = req.body.section
+    }
+    if(req.body.classe != null) {
+        res.eleve.classe = req.body.classe
+    }
+    if(req.body.pseudo != null) {
+        res.eleve.pseudo = req.body.pseudo
+    }
+    if(req.body.mdp != null) {
+        res.eleve.mdp = req.body.mdp
+    }
+    if(req.body.elo != null) {
+        res.eleve.elo = req.body.elo
+    }
+    if(req.body.maison != null) {
+        res.eleve.maison = req.body.maison
+    }
+    if(req.body.nbPartiesJoue != null) {
+        res.eleve.nbPartiesJoue = req.body.nbPartiesJoue
+    }
+    if(req.body.nbPartiesGagne != null) {
+        res.eleve.nbPartiesGagne = req.body.nbPartiesGagne
+    }
+    if(req.body.idEcole != null) {
+        res.eleve.idEcole = req.body.idEcole
+    }
+   try {
+    const updatedEleve = await res.eleve.save()
+    res.json(updatedEleve)
+   } catch (err) {
+    res.status(400).json({ message: err.message })
+   }
 })
 
 //Delete One
-router.delete('/:id',  (req, res) => {
-   
+router.delete('/:id', getEleve, async (req, res) => {
+   try {
+    await res.eleve.remove()
+    res.json({ message: 'Deleted Eleve'})
+   } catch (err) {
+    res.status(500).json({ message: err.message })
+   }
 })
 
+async function getEleve(req, res, next) {
+    let eleve
+    try {
+        eleve = await Eleve.findById(req.params.id)
+        if (eleve == null) {
+            return res.status(404).json({message: 'Cannot find eleve'})
+        }
+    } catch (err) {
+        return res.status(500).json({message: err.message})
+    }
 
+    res.eleve = eleve
+    next()
+}
 
 
 module.exports = router
